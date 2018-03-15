2f0c3f90dbe0f5b2985e052f74bb1d50f0d9018f
Kasper Pickett <kasperpickett@gmail.com>
2018-03-15 00:53:40 -0500

    checkpoint 20 sliders revised

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -40,13 +40,8 @@
       $('#time-control input').val(percent);
     }, 1000);
 
-    function secondsTimeSpanToHMS(s) {
-    var h = Math.floor(s/3600); //Get whole hours
-    s -= h*3600;
-    var m = Math.floor(s/60); //Get remaining minutes
-    s -= m*60;
-    return h+":"+(m < 10 ? '0'+m : m)+":"+(s < 10 ? '0'+s : s); //zero padding on minutes and seconds
-}
+    $('#volume-control input').on('input', function (event) {
+      player.setVolume(event.target.value);
+    });
 
-secondsTimeSpanToHMS(currentTime);
 }
